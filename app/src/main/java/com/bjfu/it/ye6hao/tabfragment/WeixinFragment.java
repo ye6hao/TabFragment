31c47031926a9a68b771524d7376e958bba27e95
ye6hao <245419667@qq.com>
2016-08-29 14:56:11 +0800

    Initial commit

---
+++ app/src/main/java/com/bjfu/it/ye6hao/tabfragment/WeixinFragment.java
@@ -0,0 +1,17 @@
+package com.bjfu.it.ye6hao.tabfragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeixinFragment extends Fragment
+{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		return inflater.inflate(R.layout.tab01, container, false);
+	}
+}
